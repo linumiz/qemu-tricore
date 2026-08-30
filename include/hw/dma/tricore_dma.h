51685b46ce7c5f4ac2d28b35126a292eaf7eb586
Bytewolf <noreply@users.noreply.github.com>
2026-08-30 08:22:10 +0200

    add TriCore DMA public device header

---
+++ include/hw/dma/tricore_dma.h
@@ -0,0 +1,16 @@
+#ifndef HW_DMA_TRICORE_DMA_H
+#define HW_DMA_TRICORE_DMA_H
+
+#include "hw/core/sysbus.h"
+
+#define TYPE_TRICORE_DMA "tricore-dma"
+OBJECT_DECLARE_SIMPLE_TYPE(TriCoreDMAState, TRICORE_DMA)
+
+struct TriCoreDMAState {
+    SysBusDevice parent_obj;
+    MemoryRegion iomem;
+    qemu_irq irq;
+    uint32_t src, dst, length, control, status;
+};
+
+#endif
