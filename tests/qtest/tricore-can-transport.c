16b41b64a846d5773ec36297bea9bac41d08596d
Bytewolf <noreply@users.noreply.github.com>
2026-08-30 04:20:37 +0200

    Add TC277 CAN transport qtest

---
+++ tests/qtest/tricore-can-transport.c
@@ -0,0 +1,16 @@
+#include "qemu/osdep.h"
+#include "libqtest-single.h"
+
+static void test_tc277_can_machine(void)
+{
+    qtest_start("-object can-bus,id=canbus "
+                "-machine KIT_AURIX_TC277_TRB,canbus=canbus");
+    qtest_quit(global_qtest);
+}
+
+int main(int argc, char **argv)
+{
+    g_test_init(&argc, &argv, NULL);
+    g_test_add_func("/tricore/can-machine", test_tc277_can_machine);
+    return g_test_run();
+}
